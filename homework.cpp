80d9495e0bf74d55351c19b90672d42951f89a91
cdnguyen125 <cdnguyen125@student.hbuhsd.edu>
2025-01-14 21:49:14 +0000

    it works!

--- homework.cpp
+++ homework.cpp
@@ -11,10 +11,28 @@
 #include <string>    // std::string, std::getline
 #include <vector>    // std::vector
 
+void compare(int n1, int n2)
+    {
+        if (n1 > n2)
+        {
+            std::cout << n1 << " is greater than " << n2;
+        }
+          else 
+               if (n1 < n2)
+               {
+                 std::cout << n1 << " is smaller than " << n2;
+               }
+            else
+                std::cout << "the inputs are the same";
+        
+
+    }
+
+
 int main()
 {
     // Complete your homework here
-
+    compare(1, 5);
     
     return 0;
 }
