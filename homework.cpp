7fb44162b9ae2f637fbca04307699a29de5f406b
cdnguyen125 <cdnguyen125@student.hbuhsd.edu>
2025-01-15 21:05:11 +0000

    finished

--- homework.cpp
+++ homework.cpp
@@ -10,6 +10,7 @@
 #include <iostream>  // std::cin, std::cout
 #include <string>    // std::string, std::getline
 #include <vector>    // std::vector
+#include <cmath>
 
 void compare(int n1, int n2)
     {
@@ -23,16 +24,61 @@ void compare(int n1, int n2)
                  std::cout << n1 << " is smaller than " << n2;
                }
             else
-                std::cout << "the inputs are the same";
+                std::cout << "the inputs are equal";
         
-
+        
+        std::cout << std::endl;
     }
+
+void quadForm (double a, double b, double c)
+{
+    double posAns = (b*-1 + sqrt(b*b -(4*a*c)))/2*a;
+    double negAns = (b*-1 - sqrt(b*b -(4*a*c)))/2*a;
+    std::cout << "the two factors of the quadratic function " << a <<"x^2 +" << b << "x +" << c << " is: " << posAns << ", and " << negAns << std::endl;
+}
+
+void minList (std::vector<int> List)
+{
+    int index = 0;
+    int min = List[0];
+    while (index < List.size())
+    {
+        if (List[index] < min)
+        {
+            min = List[index];
+        }
+        index = index+1;
+    }
+    std::cout << "The minimum value of the list is: " << min << std::endl;
+}
 
 
 int main()
 {
-    // Complete your homework here
-    compare(1, 5);
-    
+    // 1.
+    int compare1;
+    int compare2;
+    std::cout << "type in two numbers to compare." << std::endl;
+    std:: cin >> compare1 >> compare2;
+    compare(compare1, compare2);
+    std::cout << std::endl;
+
+    // 2.
+    double quadForm_a;
+    double quadForm_b;
+    double quadForm_c;
+    std::cout << "type in three values for a, b, and c for the formula y = ax^2 + bx + c to find its two x values." << std::endl;
+    std::cin >> quadForm_a >> quadForm_b >> quadForm_c; 
+    quadForm(quadForm_a, quadForm_b, quadForm_c);
+    std::cout << std::endl;
+
+    //3. 
+    std::vector <int> listA = {1, 2, 3};
+    std::vector <int> listB = {4, 5, 6};
+    std::vector <int> listC = {7, 8, 9};
+    minList(listA);
+    minList(listB);
+    minList(listC);
+
     return 0;
 }
